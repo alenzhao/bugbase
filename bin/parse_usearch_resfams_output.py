28f689b3b8279a846df37c3307ca3b933c1f5a00
danknights <danknights@gmail.com>
2014-08-08 15:14:08 -0500

    added missing comma

--- bin/parse_usearch_resfams_output.py
+++ bin/parse_usearch_resfams_output.py
@@ -16,7 +16,7 @@ def get_opts():
         default=None, help="Input usearch output file [required].")
     p.add_option("-o", "--output", type="string", \
         default=None, help="Output file [required].")
-    p.add_option("-r" "--resfams_map", type="string", \
+    p.add_option("-r", "--resfams_map", type="string", \
         default=None, help="tab-delimited and have resfams category in column 1 and gene name in column 2 [required]")
     p.add_option("--verbose", action="store_true", \
         help="Print all output.")
@@ -31,6 +31,8 @@ def check_opts(opts):
 		raise ValueError('\n\nPlease include an output file.')
 	if opts.resfams_map is None:
 		raise ValueError('\n\nPlease include a resfams map.')
+
+
 
 if __name__ == '__main__':
 	opts, args = get_opts()

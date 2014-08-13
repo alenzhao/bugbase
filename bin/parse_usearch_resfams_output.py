3a56cd4c459f51567c5542cf3d9b4d5b189f23ce
Tonya Ward <tward@teraminx.cs.umn.edu>
2014-08-13 09:50:35 -0500

    creates an output file with resfams_ID as columns and img_ID rows, 1 if gene is present, 0 if not present

--- bin/parse_usearch_resfams_output.py
+++ bin/parse_usearch_resfams_output.py
@@ -1,4 +1,4 @@
-# Parse usearch output and map to RESFAMS genes/categories
+# Parse usearch output and map to resfams genes/categories
 # 
 # Note: --resfams_map should be tab-delimited and have resfams category in column 1
 # and gene name in column 2
@@ -38,21 +38,53 @@ if __name__ == '__main__':
 	opts, args = get_opts()
 	check_opts(opts)
 	
-        #Open input file
+        # open input file
         input_file = open(opts.input, "r")
         
-        # a dict mapping img IDs to a list of RESFAMS_IDs
+        # a dict mapping img IDs to a list of resfams_IDs
         genes = {}
+
+        # a dict of all resfams IDs (no duplicates)
+        resfams_ID_list = []
 
         for line in input_file:
             words = line.strip().split("\t")
             img_ID = words[0].split("_")[0]
-            RESFAMS_ID = words[1]
+            resfams_ID = words[1]
+
+            # for each line check to see if
+            # the img_ID is in 'genes',if not, add 
+            # Then for that given img_ID append
+            # resfams_ID on that line for the given img_ID
             if not genes.has_key(img_ID):
                 genes[img_ID] = []
-            genes[img_ID].append(RESFAMS_ID)
+            genes[img_ID].append(resfams_ID)
+
+            # for each line check if resfams_ID 
+            # is in resfams_ID_list, if not, add
+            if not resfams_ID in resfams_ID_list:
+                resfams_ID_list.append(resfams_ID)
+
+        # create an output file and open it 
+        output_file = open(opts.output, "w")
         
-        print len(genes)
-        
+        # in the output file write the resfams_ID_list
+        # with each ID seperated by tabs
+        for resfams_ID in resfams_ID_list:
+           output_file.write(resfams_ID + "\t")
+
+        # for each img_ID in genes
+        # print a new line with the ID and tab
+        # for each resfams_ID print:
+        # 1 + tab if present in genes[img_ID]
+        # or 0 + tab if not present
         for img_ID in genes:
-            print img_ID, len(genes[img_ID])
+            output_file.write("\n" + img_ID + "\t")
+            for resfams_ID in resfams_ID_list:
+                if resfams_ID in genes[img_ID]:
+                    output_file.write("1\t")
+                else:
+                    output_file.write("0\t")
+
+        # close the output file
+        output_file.close
